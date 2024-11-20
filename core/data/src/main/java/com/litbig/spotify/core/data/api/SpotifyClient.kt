768911f87c41f0087b6793bee52db23ab85d7695
Jacob Kang <jacob@litbig.com>
2024-11-20 18:17:19 +0900

    auth handshake error fix

--- core/data/src/main/java/com/litbig/spotify/core/data/api/SpotifyClient.kt
+++ core/data/src/main/java/com/litbig/spotify/core/data/api/SpotifyClient.kt
@@ -1,12 +1,18 @@
 package com.litbig.spotify.core.data.api
 
+import okhttp3.ConnectionSpec
 import okhttp3.OkHttpClient
+import okhttp3.TlsVersion
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object SpotifyClient {
+    private val spec = ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+        .tlsVersions(TlsVersion.TLS_1_2, TlsVersion.TLS_1_3)
+        .build()
     private val client = OkHttpClient.Builder()
+        .connectionSpecs(listOf(spec))
         .addInterceptor(HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         })
@@ -34,6 +40,7 @@ object SpotifyClient {
             _authRetrofit ?: Retrofit.Builder()
                 .baseUrl(AUTH_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build().also {
                     _authRetrofit = it
                 }
